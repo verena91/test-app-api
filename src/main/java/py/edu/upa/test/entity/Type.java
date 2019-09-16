c98fae10a07d15d44ac3cb1be26f385192186541
Paloma <paloma.britez@upa.edu.py>
2019-09-16 14:03:40 -0400

    2

---
+++ src/main/java/py/edu/upa/test/entity/Type.java
@@ -0,0 +1,51 @@
+package py.edu.upa.test.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity
+@Table(name="type", schema="public")
+public class Type implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    @SequenceGenerator(name = "type_id_seq", sequenceName = "type_id_seq", allocationSize = 1, schema= "public")
+    @GeneratedValue(generator = "type_id_seq")
+	private Integer id;
+	
+	@Column(name="nombre")
+	private String nombre;
+	
+	@Column(name="descripcion")
+	private String descripcion;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	@Column(name="delete")
+	private boolean delete;
+
+	public boolean isDelete() {
+		return delete;
+	}
+
+	public void setDelete(boolean delete) {
+		this.delete = delete;
+	}
+
+	
+
+}
